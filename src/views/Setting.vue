c5b372f7e8462b1d526210f600ab12a96610cda8
Irene289 <esmuybonita18@gmail.com>
2022-05-14 02:21:46 +0800

    Feature: test dummyData on Setting page ok

--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -1,101 +1,225 @@
 <template>
-  <div class="container">
-    <Sidebar />
-    <form class="setting-form" action="">
-      <div class="title">
-        <h1 class="setting-form__title">帳戶設定</h1>
-      </div>
+  <main>
+    <div class="container">
+      <div class="row">
+        <!-- sidebar -->
+        <div class="col-2 sidebar">
+          <Sidebar />
+        </div>
 
-      <div class="setting-form__div setting-form__input-account">
-        <label for="">帳號</label>
-        <input type="text" name="account" placeholder="請輸入帳號" />
-      </div>
+        <!-- form -->
+        <form 
+          class="col-7 content-container setting-forms" 
+          action=""
+          @submit.stop.prevent="handleSubmit"
+        >
+          <div class="setting-form-title">
+            <h1>帳戶設定</h1>
+          </div>
 
-      <div class="setting-form__div setting-form__input-name">
-        <label for="">名稱</label>
-        <input type="text" name="name" placeholder="請輸入使用者名稱" />
-      </div>
+          <div class="setting-form setting-form__input-account">
+            <label for="">帳號</label>
+            <input 
+              v-model="user.account"
+              type="text" 
+              name="account" 
+              placeholder="請輸入帳號"
+              required 
+            />
+          </div>
 
-      <div class="setting-form__div setting-form__input-email">
-        <label for="">Email</label>
-        <input type="email" name="email" placeholder="請輸入 Email" />
-      </div>
+          <div class="setting-form setting-form__input-name">
+            <label for="">名稱</label>
+            <input 
+              v-model="user.name"
+              type="text" 
+              name="name" 
+              placeholder="請輸入使用者名稱"
+              required 
+            />
+          </div>
 
-      <div class="setting-form__div setting-form__input-password">
-        <label for="">密碼</label>
-        <input type="password" name="password" placeholder="請設定密碼" />
-      </div>
+          <div class="setting-form setting-form__input-email">
+            <label for="">Email</label>
+            <input 
+              v-model="user.email"
+              type="email" 
+              name="email" 
+              placeholder="請輸入 Email"
+              required 
+            />
+          </div>
 
-      <div class="setting-form__div setting-form__input-password-check">
-        <label for="">密碼再確認</label>
-        <input
-          type="password"
-          name="passwordCheck"
-          placeholder="請再次輸入密碼"
-        />
-      </div>
+          <div class="setting-form setting-form__input-password">
+            <label for="">密碼</label>
+            <input 
+              v-model="user.password"
+              type="password" 
+              name="password" 
+              placeholder="請設定密碼"
+              required
+            />
+          </div>
 
-      <button class="btn btn-setting" type="submit" :disabled="isProcessing">
-        儲存
-      </button>
-    </form>
-    <div></div>
-  </div>
+          <div class="setting-form setting-form__input-password-check">
+            <label for="">密碼再確認</label>
+            <input
+              v-model="user.passwordCheck"
+              type="password"
+              name="passwordCheck"
+              placeholder="請再次輸入密碼"
+              required
+            />
+          </div>
+
+          <button
+            class="btn btn-setting"
+            type="submit"
+            :disabled="isProcessing"
+          >
+            儲存
+          </button>
+        </form>
+
+        <!-- blank -->
+        <div class="col-3"></div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
-import Sidebar from '../components/Sidebar.vue'
+import Sidebar from '../components/Sidebar'
+import { Toast } from '../utils/helpers'
+
+const dummyData = {
+  currentUser: {
+    id: 1,
+    account: "root",
+    name: "root",
+    email: "root@example.com",
+    password: "$2a$10$lvYqyY7mfVRmhHlQSJjeMed8Yd3yNAsEYoY0TzurnyQzTmI90Sz4S",
+    nickname: "root",
+    coverImg: "https://picsum.photos/800/300",
+    avatarImg: "https://i.pravatar.cc/300",
+    bio: null,
+  }
+};
 
 export default {
-  name: 'Setting',
+  name: "Setting",
   components: {
-    Sidebar
-  }
+    Sidebar,
+  },
+  data () {
+    return {
+      user: {
+        id: -1,
+        account: '',
+        name: '',
+        email: '',
+        password: '',
+        passwordCheck: ''
+      },
+      isProcessing: false
+    }
+  },
+  created() {
+    const { id } = this.$route.params
+    this.getUser(id)
+  },
+  methods: {
+    getUser (userId) {
+      // TODO:使用currentUser
+      console.log('userId:', userId)
+      const { 
+        id,
+        account,
+        name,
+        email
+       } = dummyData.currentUser
+
+      this.user = {
+        id,
+        account,
+        name,
+        email
+      }
+    },
+    handleSubmit (e) {
+      // TODO:API
+      this.isProcessing = true
+
+      console.log(this.user.password)
+
+      if ( 
+          !this.user.account || 
+          !this.user.name || 
+          !this.user.email || 
+          !this.user.password || 
+          !this.user.passwordCheck
+      ) {
+        Toast.fire({
+          icon: 'warning',
+          title: '請輸入所有欄位'
+        })
+        this.isProcessing = false
+        return
+      } 
+
+      if (this.user.password !== this.user.passwordCheck) {
+        alert('密碼不一致，請再次輸入')
+        this.isProcessing = false
+      }
+
+      const form = e.target
+      const formData = new FormData(form)
+      console.log(formData)
+    }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 @import "../assets/scss/basic.scss";
 
-.container {
-  display: grid;
-  grid-template-columns: 178px 641px auto;
-  grid-template-rows: 100vh;
-  grid-gap: 24px;
+main {
+  max-width: 1140px;
+  margin: 0 auto;
 }
-.setting-form {
+.content-container {
+  height: 100vh;
+  border-left: 1px solid $border-grey;
+  border-right: 1px solid $border-grey;
+  padding: 0;
+}
+.setting-forms {
   display: flex;
   flex-flow: column nowrap;
   justify-content: flex-start;
-  align-items: flex-start;
-  border-left: 1px solid $border-grey;
-  border-right: 1px solid $border-grey;
+  align-items: center;
 }
-.title {
+.setting-form-title {
   width: 100%;
   border-bottom: 1px solid $border-grey;
   h1 {
-    font-size: 24px;
-    line-height: 26px;
-    font-weight: 700;
-    color: #171725;
-    margin: 24px auto 24px 25px;
+    @extend %heading-24;
+    margin: 24px auto 24px 24px;
   }
 }
-.setting-form__div {
+.setting-form {
   @extend %input-block;
-  width: 593px;
+  width: calc(100% - 2 * 24px);
   display: flex;
   flex-flow: column nowrap;
-  margin-left: 24px;
-  margin-right: 24px;
+  margin: 24px 24px 8px 24px;
   label {
     @extend %form-label;
   }
   input {
     @extend %form-input;
-    border: none;
     background: $form-input-grey;
+    border: none;
   }
   input::placeholder {
     color: $form-input-placeholder;
@@ -106,14 +230,11 @@ export default {
     width: 100%;
   }
 }
-.setting-form__input-account {
-  margin-top: 24px;
-}
 .btn {
   @extend %button-orange;
   width: 88px;
   height: 46px;
-  margin: 40px 24px auto auto;
+  margin: 32px 24px auto auto;
   font-size: 20px;
   line-height: 30px;
 }
