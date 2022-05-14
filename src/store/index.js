d70075b3f5d336e60e49243088969d7898c1e094
SeijoHuang <erinhuang817@gmail.com>
2022-05-15 03:31:11 +0800

    add Log out function

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+      currentUser: {
+        id: -1,
+        name: '',
+        email: '',
+        image: '',
+        isAdmin: false
+      },
+      isAuthenticated: false,
+      token: ''
   },
   getters: {
   },
   mutations: {
+    revokeAuthentication(state){
+      //清空state
+      state.currentUser = {}
+      //通過驗證改為false
+      state.isAuthenticated = false
+      //移除sate中儲存的token
+      state.token = ""
+      //移除localStoage裡儲存的
+      localStorage.removeItem('token')
+    }
   },
   actions: {
   },
