59469cbaaa4d4f2d6d6248c3bc0c43d743525b46
Irene289 <esmuybonita18@gmail.com>
2022-05-18 19:24:00 +0800

    vue.config.js revisse

--- vue.config.js
+++ vue.config.js
@@ -12,9 +12,9 @@ console.log(process.env.VUE_APP_BASEURL)
 // }
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? './'
-    : '/',
+  publicPath: process.env.NODE_ENV === "production"
+    ? "./"
+    : "/",
   configureWebpack: {
     devtool: 'source-map'
   }
