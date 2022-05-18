1a6cc497d622a3bc64d366c5a0d2fc14cd69f043
Irene289 <esmuybonita18@gmail.com>
2022-05-18 18:45:35 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,13 @@
 console.log(process.env.VUE_APP_BASEURL)
 
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/twitter-front-end-vue/'
+//     : '/'
+// }
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/twitter-front-end-vue/'
-    : '/'
+  publicPath: '/'
 }
 
 module.exports = {
