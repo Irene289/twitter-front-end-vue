87af6c944b8900351fd9f7325133c85c3ee87797
SeijoHuang <erinhuang817@gmail.com>
2022-05-21 16:28:51 +0800

    add篩選名稱輸入空格功能和彈跳警示窗

--- src/components/EditModal.vue
+++ src/components/EditModal.vue
@@ -71,6 +71,7 @@
 </template>
 <script>
 import {imgFilter} from '../utils/mixins'
+import {Toast} from '../utils/helpers'
 export default {
   props:{
     initialUserEdit:{
@@ -124,6 +125,15 @@ export default {
       this.user.coverImg = this.initialCover
     },
     handleSubmit(e){
+      //排除name輸入空格
+      const name = this.user.name.replace(/ /g,'')
+      if(name.length === 0 ){
+        Toast.fire({
+          icon: 'warning',
+          title: '名稱不可為空白'
+        }) 
+        return 
+      }
       if(this.user.name.length > 50 || this.user.introduction.length > 160) {
          return
       } 
